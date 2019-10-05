709f3ebc85f6ef731716641f65ad9abf00792bf6
Devansh Pandey <54002153+Devu2707@users.noreply.github.com>
2019-10-05 19:56:26 +0530

    code was successfully compiled and desired output was obtained.

---
+++ Question11.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Question11 {
+            public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            String sname = sc.nextLine().trim();
+            char ch = sc.next().charAt(0);
+            for (int i = 0; i <sname.length() ; i++) {
+                if (sname.charAt(i)==ch){
+                    System.out.println(i);
+                }
+            }
+        }
+   }
