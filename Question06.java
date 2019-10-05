e52c57fbd7e9cc1b98155d048c61f826b8e2f148
Devansh Pandey <54002153+Devu2707@users.noreply.github.com>
2019-10-05 19:53:35 +0530

    code was successfully compiled and desired output was obtained.

---
+++ Question06.java
@@ -0,0 +1,10 @@
+import javax.print.DocFlavor;
+import java.util.Scanner;
+
+public class Question06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String sname = sc.nextLine().trim();
+        System.out.println(sname.toUpperCase());
+    }
+}
